28dae90d52b58570d2b3612e47ca4d6c0d0eb522
Rachele Morino <rachelekm@gmail.com>
2018-04-24 16:37:59 -0400

    mongoose install, configuration, update runServer and closeServer to connect to database

--- server.js
+++ server.js
@@ -20,35 +20,44 @@ app.use(morgan('common'));
 
 app.use('/blog-posts', blogPostRouter);
 
+const {PORT, DATABASE_URL} = require('./config');
+mongoose.connect(DATABASE_URL);
 let server;
 
-function runServer() {
-  const port = process.env.PORT || 8080;
+function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err)
+    mongoose.connect(databaseUrl, err => {
+      if(err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+    })
+    .on('error', err => {
+      mongoose.disconnect();
+      reject(err);
+    });
     });
   });
 }
 
 function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve();
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
     });
   });
 }
 
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+  runServer(DATABASE_URL).catch(err => console.error(err));
 };
 
 module.exports = {app, runServer, closeServer};
