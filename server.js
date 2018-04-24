e1332fcbdebb9f2d2b2fa810de9a189f51f20598
Rachele Morino <rachelekm@gmail.com>
2018-04-24 16:08:33 -0400

    config.js creation

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
 
 const jsonParse = bodyParser.json();
 const app = express();
